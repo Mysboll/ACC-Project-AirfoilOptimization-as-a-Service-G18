34c4196ae988a46fb29a5e2e9f4361d56470ff63
Tor Berglind <berglind.tor@gmail.com>
2018-10-29 14:26:37 +0100

    Updated files from hard code -> varibles

--- airfoil_task.sh
+++ airfoil_task.sh
@@ -8,8 +8,8 @@ viscosity="$5"
 velocity="$4"
 duration="$7"
 
-docker exec -it d08a03148001 murtazo/cloudnaca/runme.sh $angle $angle 1 $n_nodes $n_levels
+docker exec -it airfoil murtazo/cloudnaca/runme.sh $angle $angle 1 $n_nodes $n_levels
 
-docker exec -it d08a03148001 dolfin-convert murtazo/cloudnaca/msh/r${n_levels}a${angle}n${n_nodes}.msh murtazo/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
+docker exec -it airfoil dolfin-convert murtazo/cloudnaca/msh/r${n_levels}a${angle}n${n_nodes}.msh murtazo/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
 
-docker exec -it d08a03148001 murtazo/navier_stokes_solver/airfoil ${num_samples} ${viscosity} ${velocity} ${duration}. murtazo/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
+docker exec -it airfoil murtazo/navier_stokes_solver/airfoil ${num_samples} ${viscosity} ${velocity} ${duration}. murtazo/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
--- docker_call.py
+++ docker_call.py
@@ -6,11 +6,9 @@ app = Celery('tasks', backend='amqp', broker='pyamqp://user:password@192.168.1.3
 @app.tasks
 def airfoil_simulation(angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration):
     subprocess.run(["./airfoil_task.sh", angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration])
-    results = subprocess.run(['docker', 'exec', '-it', 'd08a03148001', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
+    results = subprocess.run(['docker', 'exec', '-it', 'airfoil', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
     print(results.stdout)
     return results.stdout
 
 @app.tasks
 def sumcalc():
-
-#airfoil_simulation('5', '200', '1', '10', '0.0001', '10', '0.1)
