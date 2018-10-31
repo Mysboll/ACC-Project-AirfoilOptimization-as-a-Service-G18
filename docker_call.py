63b666eaf514439fbea3c9a6ad8a22c621b7c943
Tor Berglind <berglind.tor@gmail.com>
2018-10-31 15:55:41 +0100

    fixed bug

--- docker_call.py
+++ docker_call.py
@@ -3,7 +3,7 @@ import subprocess
 
 app = Celery('tasks', backend='amqp', broker='pyamqp://user:password@192.168.1.59:5672/vhost')
 
-@app.tasks
+@app.task
 def airfoil_simulation(angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration):
     subprocess.run(["./airfoil_task.sh", angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration])
     results = subprocess.run(['docker', 'exec', '-it', 'airfoil', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
