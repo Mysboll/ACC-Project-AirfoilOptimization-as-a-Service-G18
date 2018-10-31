9c2938204fc2e462caede5930168b2d016e0619a
Tor Berglind <berglind.tor@gmail.com>
2018-10-31 16:51:21 +0100

    scaling down docker call

--- docker_call.py
+++ docker_call.py
@@ -6,6 +6,6 @@ app = Celery('tasks', backend='amqp', broker='pyamqp://user:password@192.168.1.5
 @app.task
 def airfoil_simulation(angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration):
     subprocess.run(["./airfoil_task.sh", angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration])
-    results = subprocess.run(['docker', 'exec', '-it', 'airfoil', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
-    print(results.stdout)
-    return results.stdout
+    #results = subprocess.run(['docker', 'exec', '-it', 'airfoil', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
+    #print(results.stdout)
+    return ("done")
