9e6a49760d953bfef8982a48f2df878ce4b344cc
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2018-11-01 11:27:39 +0100

    Added Comments

--- docker_call.py
+++ docker_call.py
@@ -6,6 +6,7 @@ app = Celery('tasks', backend='amqp', broker='pyamqp://user:password@192.168.1.1
 @app.task
 def airfoil_simulation(angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration):
     subprocess.run(["./airfoil_task.sh", angle, n_nodes, n_levels, num_samples, viscosity, velocity, duration])
+    # Use subprocess.Popen(["some script"]) so that we can use Popen.wait to ask python to wait for the subprocess to complete before it runs another one from the queue
     #results = subprocess.run(['docker', 'exec', '-it', 'airfoil', 'cat', 'results/drag_ligt.m'], stdout=subprocess.PIPE)
     #print(results.stdout)
     return ("done")
