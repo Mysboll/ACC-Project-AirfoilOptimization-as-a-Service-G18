1dcdaa672a1f1eea34a65a52f33bd2e42c9656c2
Tor Berglind <berglind.tor@gmail.com>
2018-11-01 11:25:05 +0100

    fixed path

--- airfoil_task.sh
+++ airfoil_task.sh
@@ -8,8 +8,8 @@ viscosity="$5"
 velocity="$4"
 duration="$7"
 
-docker exec -it airfoil AirFoil/cloudnaca/runme.sh $angle $angle 1 $n_nodes $n_levels
+docker exec -it airfoil /AirFoil/cloudnaca/runme.sh $angle $angle 1 $n_nodes $n_levels
 
-docker exec -it airfoil dolfin-convert AirFoil/cloudnaca/msh/r${n_levels}a${angle}n${n_nodes}.msh AirFoil/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
+docker exec -it airfoil dolfin-convert /AirFoil/cloudnaca/msh/r${n_levels}a${angle}n${n_nodes}.msh /AirFoil/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
 
-docker exec -it airfoil AirFoil/navier_stokes_solver/airfoil ${num_samples} ${viscosity} ${velocity} ${duration}. AirFoil/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
+docker exec -it airfoil /AirFoil/navier_stokes_solver/airfoil ${num_samples} ${viscosity} ${velocity} ${duration}. /AirFoil/cloudnaca/msh/r${levels}a${angle}n${n_nodes}.xml
